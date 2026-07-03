98dd233264e5ce78854f8f933b1fe81d94906294
Tushar Sharma <tusharsharmar46665@gmail.com>
2026-07-03 13:51:02 +0530

    print list using while loop

---
+++ variables/sets/loops/practicewhile.py
@@ -0,0 +1,13 @@
+# print the list using loop
+nums=[1,4,6,7,8,9]
+index=0
+while index<=len(nums)-1:
+    print(nums[index])
+    index+=1
+
+#print name of list using loop
+names=["tushar","sachin","rohit","virat"]
+index=0
+while index<=len(names)-1:
+    print(names[index])
+    index+=1
