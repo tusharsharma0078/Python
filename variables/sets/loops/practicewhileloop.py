06e6c9d58771369e65b3de5da0b05c65ecacb44c
Tushar Sharma <tusharsharmar46665@gmail.com>
2026-07-03 13:18:00 +0530

    Add while loop examples for printing numbers and multiplication table

---
+++ variables/sets/loops/practicewhileloop.py
@@ -0,0 +1,20 @@
+#print number from 1 to 100.
+i=1
+while i<=100:
+    print(i)
+    i+=1
+#print number from 100 to 1.
+
+i=100
+while i>=1:
+    print(i)
+    i-=1
+
+#print the multiplication table of n
+
+n=int(input("Enter the number to print multiplication table: ")) 
+i=1
+while i<=10:
+   print(n*i)
+   i+=1
+   
