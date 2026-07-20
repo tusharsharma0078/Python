29cda9020972ceb90d8d6c91a335554e102b1bd1
Tushar Sharma <tusharsharmar46665@gmail.com>
2026-07-20 15:50:24 +0530

    r+(read and write)

---
+++ variables/conditionals/operators/files/file_i/r+.py
@@ -0,0 +1,7 @@
+# IN the r+ we read and write together
+from pathlib import Path
+with open(Path(__file__).parent/"newfile.txt","r+")as abc:
+    print(abc.read())
+    abc.write("\n and my friend name is Vivek")
+ #with this method we can read and write the file together without deleting
+ #existing content
